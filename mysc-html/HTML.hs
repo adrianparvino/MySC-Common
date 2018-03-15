5f4deefc9c742278f6cf5f170945d833e9fd1503
Adrian Parvin D. Ouano <adrianparvino@gmail.com>
2018-03-15 16:50:52 +0800

    Wip

--- mysc-html/HTML.hs
+++ mysc-html/HTML.hs
@@ -21,4 +21,6 @@ import qualified Text.Blaze.Html5 as H
 import qualified Text.Blaze.Html5.Attributes as A
 
 html :: H.Html
-html = H.nav H.! A.class_ "navbar is-fixed-top" $ H.toHtml ("My System for Comments" :: String)
+html = do
+  H.nav H.! A.class_ "navbar is-fixed-top" $ H.toHtml ("My System for Comments" :: String)
+  H.div H.! A.id "comments" $ return ()
